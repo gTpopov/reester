75effb4615e9c13927156560695e874f37c767cf
gTpopov <greghiworld@icloud.com>
2014-08-15 12:10:06 +0300

    merge

--- protected/views/index/index.php
+++ protected/views/index/index.php
@@ -3,7 +3,40 @@
         ->registerScriptFile('/js/application/index/index.js');
 ?>
 
-    <div class="add-obj-fl-ap-nw-sale">
-
+    <div id="add-obj-fl-ap-nw-sale" class="col-sm-12">
+        <div class="col-sm-12">
+            <div class="row">
+                <div class="col-sm-3">
+                    <div class="row">
+                        <div class="btn-group" data-toggle="buttons">
+                            <label class="btn btn-primary active">
+                                <input type="radio" name="options" id="option1" checked> Купить
+                            </label>
+                            <label class="btn btn-primary">
+                                <input type="radio" name="options" id="option2"> Арендовать
+                            </label>
+                            <label class="btn btn-primary">
+                                <input type="radio" name="options" id="option3"> Оценить
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="row">
+                        <div class="btn-group" data-toggle="buttons">
+                            <label class="btn btn-primary active">
+                                <input type="radio" name="options" id="option1" checked> Купить
+                            </label>
+                            <label class="btn btn-primary">
+                                <input type="radio" name="options" id="option2"> Арендовать
+                            </label>
+                            <label class="btn btn-primary">
+                                <input type="radio" name="options" id="option3"> Оценить
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
