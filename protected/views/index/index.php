e72fd9df0cba02c1c6223e03d440bb5bbea508fb
gTpopov <greghiworld@icloud.com>
2014-08-16 00:31:18 +0300

    index filter remake

--- protected/views/index/index.php
+++ protected/views/index/index.php
@@ -4,9 +4,10 @@
 ?>
 
     <div id="add-obj-fl-ap-nw-sale" class="col-sm-12">
+
         <div class="col-sm-12">
             <div class="row padding-horizontal-10-px">
-                <div class="col-sm-12"><div class="row"><h2 style="margin-top: 0; font-weight: 200; border-bottom: 1px solid #DDD; padding-bottom: 5px">Основные параметры фильтра</h2></div></div>
+                <div class="col-sm-12"><div class="row"><h2 class="text-left" style="margin-top: 0; font-weight: 200; border-bottom: 1px solid #DDD; padding-bottom: 5px">Основные параметры фильтра</h2></div></div>
                 <div class="col-sm-12">
                     <div class="row">
                         <div class="col-sm-2">
@@ -53,7 +54,6 @@
                                                 <li><span data-value="5">неделя</span></li>
                                                 <li><span data-value="6">месяц</span></li>
                                             </ul>
-                                            <input type="hidden" id="selected-time-to-underground-station" />
                                             <?php //echo $form->hiddenField($modelH,'metro_way',array('id'=>'selected-underground-station','value'=>0)); ?>
                                         </div>
                                     </div>
@@ -181,5 +181,181 @@
                 </div>
             </div>
         </div>
+
+        <div class="col-sm-12" style="margin-top: 10px">
+            <div class="row padding-horizontal-10-px">
+                <div class="col-sm-12">
+                    <div class="row">
+                        <h2 class="text-left" style="margin-top: 0; font-weight: 200; border-bottom: 1px solid #DDD; padding-bottom: 15px">Дополнительные параметры фильтра
+                            <small class="pull-right btn btn-info btn-sm" style="font-size: 14px; margin-top: 7px; cursor: pointer"> отобразить <span style="vertical-align: -1px; font-size: 12px" class="glyphicon glyphicon-arrow-down"></span></small>
+                        </h2>
+                    </div>
+                </div>
+
+                <div class="col-sm-12">
+                    <div class="row">
+                        <div class="col-sm-2">
+                            <div class="row">
+                                <h4 style="margin-top: 0; font-weight: 300" class="text-center">материал стен</h4>
+                                <div class="btn-group" style="width: 100%; margin-bottom: 5px" data-toggle="buttons">
+                                    <label class="btn btn-sm btn-block btn-primary">
+                                        <input type="radio" name="options" id="option3"> панель
+                                    </label>
+                                </div>
+                                <div class="btn-group" style="width: 100%; margin-bottom: 5px" data-toggle="buttons">
+                                    <label class="btn btn-sm btn-block btn-primary">
+                                        <input type="radio" name="options" id="option3"> дерево
+                                    </label>
+                                </div>
+                                <div class="btn-group" style="width: 100%; margin-bottom: 5px" data-toggle="buttons">
+                                    <label class="btn btn-sm btn-block btn-primary active">
+                                        <input type="radio" name="options" id="option1" checked=""> кирпичный
+                                    </label>
+                                </div>
+                                <div class="btn-group" style="width: 100%; margin-bottom: 5px" data-toggle="buttons">
+                                    <label class="btn btn-sm btn-block btn-primary">
+                                        <input type="radio" name="options" id="option2"> монолитный
+                                    </label>
+                                </div>
+                                <div class="btn-group" style="width: 100%; margin-bottom: 5px" data-toggle="buttons">
+                                    <label class="btn btn-sm btn-block btn-primary">
+                                        <input type="radio" name="options" id="option3"> природный кемень
+                                    </label>
+                                </div>
+                                <div class="btn-group" style="width: 100%; margin-bottom: 5px" data-toggle="buttons">
+                                    <label class="btn btn-sm btn-block btn-primary">
+                                        <input type="radio" name="options" id="option3"> кирпично-монолитный
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <hr style="margin: 2px 0 5px 0; border-color: #ddd">
+                            </div>
+                            <div class="row">
+                                <div class="btn-group" style="width: 100%; margin-bottom: 5px" data-toggle="buttons">
+                                    <label class="btn btn-sm btn-block btn-warning">
+                                        <input type="radio" name="options" id="option3"> материал либого типа
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-2 col-sm-offset-1">
+                            <div class="row">
+                                <h4 style="margin-top: 0; font-weight: 300" class="text-center">выбор этажа</h4>
+                                <div class="btn-group" style="margin-bottom: 5px; width: 100%" data-toggle="buttons">
+                                    <label class="btn btn-sm btn-primary" style="width: 50%">
+                                        <input type="radio" name="options" id="option3"> не первый
+                                    </label>
+                                    <label class="btn btn-sm btn-primary" style="width: 50%">
+                                        <input type="radio" name="options" id="option3"> не последний
+                                    </label>
+                                </div>
+                                <div>
+                                    <div class="row"><span style="margin-top: 4px; font-weight: 300; width: 100%; font-size: 13px; margin-bottom: 2px" class="text-center col-sm-12">укажите желаемую этажность</span></div>
+                                    <input class="form-control fm-sm" placeholder="нап: от 12" style="margin-bottom: 4px; width: 49%; float: left">
+                                    <input class="form-control fm-sm" placeholder="нап: до 24" style="margin-bottom: 4px; width: 49%; margin-left: 1%;  float: left">
+                                </div>
+                                <div class="contain-slct smm-sl">
+                                    <div id="search-object-state-settings" class="select-int">
+                                        <div class="select">
+                                            <a href="javascript:;" class="slct">состояние объекта</a>
+                                            <ul class="drop">
+                                                <li><span data-value="0">любое</span></li>
+                                                <li><span data-value="1">дизайнпроект</span></li>
+                                                <li><span data-value="2">отличное состояние</span></li>
+                                                <li><span data-value="4">среднее состояние</span></li>
+                                                <li><span data-value="3">свежий ремонт</span></li>
+                                                <li><span data-value="8">косметический ремонт</span></li>
+                                                <li><span data-value="6">первичная отделка</span></li>
+                                                <li><span data-value="5">без отделки</span></li>
+                                                <li><span data-value="7">требует ремонт</span></li>
+                                            </ul>
+                                            <?php //echo $form->hiddenField($modelH,'metro_way',array('id'=>'selected-underground-station','value'=>0)); ?>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <hr style="margin: 5px 0 8px 0; border-color: #ddd">
+                            </div>
+                            <div class="row">
+                                <div class="btn-group" style="width: 100%; margin-bottom: 5px" data-toggle="buttons">
+                                    <label class="btn btn-sm btn-block btn-warning">
+                                        <input type="radio" name="options" id="option3"> любая этажность
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <hr style="margin: 2px 0 6px 0; border-color: #ddd">
+                            </div>
+                            <div class="row">
+                                <div class="btn-group" style="margin-bottom: 5px; width: 100%" data-toggle="buttons">
+                                    <label class="btn btn-sm btn-primary" style="width: 100%;">
+                                        <input type="radio" name="options" id="option3"> ипотека
+                                    </label>
+                                </div>
+                                <div class="btn-group" style="margin-bottom: 5px; width: 100%" data-toggle="buttons">
+                                    <label class="btn btn-sm btn-primary" style="width: 100%;">
+                                        <input type="radio" name="options" id="option3"> акции и скидки
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-2 col-sm-offset-1">
+                            <div class="row">
+                                <h4 style="margin-top: 0; font-weight: 300" class="text-center">санузел</h4>
+                                <div class="btn-group" style="margin-bottom: 5px; width: 100%" data-toggle="buttons">
+                                    <label class="btn btn-sm btn-primary" style="width: 100%;">
+                                        <input type="radio" name="options" id="option3"> 2 + санузла
+                                    </label>
+                                </div>
+                                <div class="btn-group" style="margin-bottom: 5px; width: 100%" data-toggle="buttons">
+                                    <label class="btn btn-sm btn-primary" style="width: 100%;">
+                                        <input type="radio" name="options" id="option3"> раздельный
+                                    </label>
+                                </div>
+                                <div class="btn-group" style="margin-bottom: 5px; width: 100%" data-toggle="buttons">
+                                    <label class="btn btn-sm btn-primary" style="width: 100%;">
+                                        <input type="radio" name="options" id="option3"> совмещенный
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <hr style="margin: 0 0 3px 0; border-color: #ddd">
+                            </div>
+                            <div class="row">
+                                <div class="btn-group" style="width: 100%; margin-bottom: 5px" data-toggle="buttons">
+                                    <label class="btn btn-sm btn-block btn-warning">
+                                        <input type="radio" name="options" id="option3"> любой санузел
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <hr style="margin: -1px 0 0 0; border-color: #ddd">
+                            </div>
+                            <div class="row" style="margin-top: 3px">
+                                <div class="btn-group" style="margin-bottom: 5px; width: 100%" data-toggle="buttons">
+                                    <label class="btn btn-sm btn-primary" style="width: 100%;">
+                                        <input type="radio" name="options" id="option3"> закрытая территория
+                                    </label>
+                                </div>
+                                <div class="btn-group" style="margin-bottom: 5px; width: 100%" data-toggle="buttons">
+                                    <label class="btn btn-sm btn-primary" style="width: 100%;">
+                                        <input type="radio" name="options" id="option3"> клубного типа
+                                    </label>
+                                </div>
+                                <div class="btn-group" style="margin-bottom: 5px; width: 100%" data-toggle="buttons">
+                                    <label class="btn btn-sm btn-primary" style="width: 100%;">
+                                        <input type="radio" name="options" id="option3"> балкон / лоджия
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
     </div>
 
