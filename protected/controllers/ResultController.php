3885e44dc4d327f83731ed0094072e1223b9c39a
alex-76 <mih_76@mail.ru>
2014-08-17 11:31:34 +0400

    # Filter edit ---

--- protected/controllers/ResultController.php
+++ protected/controllers/ResultController.php
@@ -44,34 +44,48 @@ class ResultController extends Controller {
         $_POST['balcony']      = 1;    // 1-лоджия или балкон +
         $_POST['parking']      = 1;    // 1-паркинг +
         $_POST['place_cars']   = 1;    // 1-машиноместо +
-        $_POST['covered_space']= 1;    // 1-закрытая (огражденная) територия
-        $_POST['club_type']    = 1;    // 1-клубного типа
-        $_POST['discount']     = 1;    // 1-акции и скидки
-        $_POST['mortgage']     = 1;    // 1-для ипотеки
-        $_POST['sanitare']     = 2;    // 1-раздельный 2-совмещенный 3-2+ санузла
-        $_POST['create_data']  = '2014-08-14'; // Искать за (все время,сегодня,1 день,3 дня,неделя,месяц
-        $_POST['deadline']     = 2017; // срок сдачи (н/р 2017)
-        $_POST['developer']    = 'LTD House Big'; // застройщик
-        $_POST['fz_214']       = 1;    // регистрация новостройки
-        $_POST['finished']     = 1;    // с отделкой
-        $_POST['photos']       = 1;    // 1-с фото, 0-отсутствие фото
-        $_POST['floors']          = 15;   // этажность дома
-        $_POST['city']            = 1;    // город
-        $_POST['district']        = 7;    // округ
-        $_POST['region']          = 11;   // регион
-        $_POST['street']          = 1001; // улица
-        $_POST['undeground']      = 9;    // метро
-        $_POST['metro_time']      = 15;   // до метро
-        $_POST['metro_way']       = 1;    // 1-пешком, 2-транспортом
-        $_POST['class_home']      = 1;    // 1-эконом 2-бизнес 3-элитный
-        $_POST['type_house']      = 1;    // 1-кирпичный 2-монолитный 3-монолитно-кирпичный 4-панельный 5-дерево 6-природный кемень
-        $_POST['type_account']    = 1;    // 1-Собственник 2-Представитель собственника 3-Риелтор
+        $_POST['covered_space']= 1;    // 1-закрытая (огражденная) територия +
+        $_POST['club_type']    = 1;    // 1-клубного типа +
+        $_POST['discount']     = 1;    // 1-акции и скидки +
+        $_POST['mortgage']     = 1;    // 1-для ипотеки +
+        $_POST['sanitare'][0]  = 1;    // 1-раздельный 2-совмещенный 3-2+ санузла +
+        $_POST['sanitare'][1]  = 2;
+        $_POST['sanitare'][2]  = 3;
+        $_POST['create_data']  = 4;    // Искать за (все время - 0, сегодня (1 день), 3 дня, неделя, месяц +
+        $_POST['deadline']     = 2017; // срок сдачи (н/р 2017) +
+        $_POST['developer']    = 1;    // застройщик +
+        $_POST['fz_214']       = 1;    // регистрация новостройки +
+        $_POST['finished']     = 1;    // с отделкой +
+        $_POST['photos']       = 1;    // 1-с фото, 0-отсутствие фото +
+        $_POST['floors']          = 15;   // этажность дома +
+        $_POST['city']            = 1;    // город +
+        $_POST['district'][0]     = 98;   // округ +
+        $_POST['district'][1]     = 97;
+        $_POST['district'][2]     = 96;
+        $_POST['region'][0]       = 73;   // регион +
+        $_POST['region'][1]       = 74;
+        $_POST['region'][2]       = 75;
+        $_POST['street']          = 1001; // улица +
+        $_POST['undeground'][0]   = 9;    // метро +
+        $_POST['undeground'][1]   = 10;
+        $_POST['undeground'][2]   = 11;
+        $_POST['metro_time']      = 15;   // до метро +
+        $_POST['metro_way'][0]    = 1;    // 1-пешком, 2-транспортом +
+        $_POST['metro_way'][1]    = 2;
+        $_POST['class_home'][0]   = 1;    // 1-эконом 2-бизнес 3-элитный +
+        $_POST['class_home'][1]   = 2;
+        $_POST['class_home'][3]   = 3;
+        $_POST['type_house']      = 1;    // 1-кирпичный 2-монолитный 3-монолитно-кирпичный 4-панельный 5-дерево 6-природный кемень +
+        $_POST['type_account'][0] = 1;    // 1-Собственник 2-Представитель собственника 3-Риелтор +
+        $_POST['type_account'][2] = 2;
+        $_POST['type_account'][3] = 3;
+
 
         ### --- END --- ###
 
 
 
-        //Сохраняем переданные параметры фильтра в сессии
+        // Сохраняем переданные параметры фильтра в сессии
         foreach($_POST as $key => $val) {
             Yii::app()->session[''.$key.''] = $val;
         }
@@ -170,46 +184,9 @@ class ResultController extends Controller {
             }
         }
 
-        // Свободная планировка, студия
-        if(isset(Yii::app()->session['plan'])) {
-
-            $plan = '';
-            foreach(Yii::app()->session['plan'] as $val_plan) {
-                $plan .= trim($val_plan.',');
-            }
-            $plan = substr($plan,0,strlen($plan)-1);
-            $condition .= " AND r.plan IN (".$plan.")";
-        }
-
-        // Окна +
-        if(isset(Yii::app()->session['window'])){
-            $condition .= " AND r.window = ".(int) Yii::app()->session['window']."";
-        }
-
-        // Балкон или лоджия +
-        if(isset(Yii::app()->session['balcony'])){
-            $condition .= " AND r.balcony = ".(int) Yii::app()->session['balcony']."";
-        }
-
-        // Паркинг +
-        if(isset(Yii::app()->session['parking'])){
-            $condition .= " AND r.parking = ".(int) Yii::app()->session['parking']."";
-        }
-
-        // Машиноместо +
-        if(isset(Yii::app()->session['place_cars'])){
-            $condition .= " AND r.place_cars = ".(int) Yii::app()->session['place_cars']."";
-        }
-
-        // Машиноместо +
-        if(isset(Yii::app()->session['covered_space'])){
-            $condition .= " AND r.covered_space = ".(int) Yii::app()->session['covered_space']."";
-        }
-
-
         //ЭТАЖНОСТЬ КВАРТИРЫ
         if(!isset(Yii::app()->session['level']) && (
-                !empty(Yii::app()->session['level_to']) ||
+                !empty(Yii::app()->session['level_to'])   ||
                 !empty(Yii::app()->session['level_from']) ||
                 isset(Yii::app()->session['level_last']) ||
                 isset(Yii::app()->session['level_first'])))
@@ -264,6 +241,203 @@ class ResultController extends Controller {
         }
 
 
+        // Свободная планировка, студия
+        if(isset(Yii::app()->session['plan'])) {
+
+            $plan = '';
+            foreach(Yii::app()->session['plan'] as $val_plan) {
+                $plan .= trim($val_plan.',');
+            }
+            $plan = substr($plan,0,strlen($plan)-1);
+            $condition .= " AND r.plan IN (".$plan.")";
+        }
+
+        // Окна +
+        if(isset(Yii::app()->session['window'])){
+            $condition .= " AND r.window = ".(int) Yii::app()->session['window']."";
+        }
+
+        // Балкон или лоджия +
+        if(isset(Yii::app()->session['balcony'])){
+            $condition .= " AND r.balcony = ".(int) Yii::app()->session['balcony']."";
+        }
+
+        // Паркинг +
+        if(isset(Yii::app()->session['parking'])){
+            $condition .= " AND r.parking = ".(int) Yii::app()->session['parking']."";
+        }
+
+        // Машиноместо +
+        if(isset(Yii::app()->session['place_cars'])){
+            $condition .= " AND r.place_cars = ".(int) Yii::app()->session['place_cars']."";
+        }
+
+        // Огороженая територия +
+        if(isset(Yii::app()->session['covered_space'])){
+            $condition .= " AND r.covered_space = ".(int) Yii::app()->session['covered_space']."";
+        }
+
+        // Клубный тип +
+        if(isset(Yii::app()->session['club_type'])){
+            $condition .= " AND r.club_type = ".(int) Yii::app()->session['club_type']."";
+        }
+
+        // Акции и скидки +
+        if(isset(Yii::app()->session['discount'])){
+            $condition .= " AND r.discount = ".(int) Yii::app()->session['discount']."";
+        }
+
+        //  Ипотека +
+        if(isset(Yii::app()->session['mortgage'])){
+            $condition .= " AND r.mortgage = ".(int) Yii::app()->session['mortgage']."";
+        }
+
+        // Санузел
+        if(isset(Yii::app()->session['sanitare'])) {
+
+            $sanitare = '';
+            foreach(Yii::app()->session['sanitare'] as $val_sanitare) {
+                $sanitare .= trim($val_sanitare.',');
+            }
+            $sanitare = substr($sanitare,0,strlen($sanitare)-1);
+            $condition .= " AND r.sanitare IN (".$sanitare.")";
+        }
+
+        // ИСКАТЬ ЗА ПЕРИОД +
+        if(isset(Yii::app()->session['create_data']) && Yii::app()->session['create_data']!=0)
+        {
+            $data = intval(Yii::app()->session['create_data']);
+            switch ($data)
+            {
+                case 1://за день (сегодня)
+                    $period = 1;
+                    break;
+                case 2://за 3 дня
+                    $period = 3;
+                    break;
+                case 3://за неделю
+                    $period = 7;
+                    break;
+                case 4://за месяц
+                    $period = 30;
+                    break;
+                default:$period = 30;
+            }
+
+            $condition .= ' AND r.create_data >=NOW()-INTERVAL '.$period.' DAY';
+        }
+
+        //  Срок сдачи +
+        if(isset(Yii::app()->session['deadline'])){
+            $condition .= " AND r.deadline = ".(int) Yii::app()->session['deadline']."";
+        }
+
+        //  Застройщик +
+        if(isset(Yii::app()->session['developer'])){
+            $condition .= " AND r.developer != '0'";
+        }
+
+        //  Регистрация нвостройки +
+        if(isset(Yii::app()->session['fz_214'])){
+            $condition .= " AND r.fz_214 = ".(int) Yii::app()->session['fz_214']."";
+        }
+
+        //  С отделкой +
+        if(isset(Yii::app()->session['finished'])){
+            $condition .= " AND r.finished = ".(int) Yii::app()->session['finished']."";
+        }
+
+        //  С фото +
+        if(isset(Yii::app()->session['photos'])){
+            $condition .= " AND r.photos = ".(int) Yii::app()->session['photos']."";
+        }
+
+        //  Этажность дома +
+        if(isset(Yii::app()->session['floors'])){
+            $condition .= " AND h.floors = ".(int) Yii::app()->session['floors']."";
+        }
+
+        //  Округ +
+        if(isset(Yii::app()->session['district'])){
+
+            $district_house = '';
+            foreach(Yii::app()->session['district'] as $val_district)
+            {
+                $district_house .= trim($val_district.',');
+            }
+            $district_house = substr($district_house,0,strlen($district_house)-1);
+            $condition .= " AND h.district IN (".$district_house.")";
+        }
+
+        //  Регион +
+        if(isset(Yii::app()->session['region'])){
+
+            $region_house = '';
+            foreach(Yii::app()->session['region'] as $val_region)
+            {
+                $region_house .= trim($val_region.',');
+            }
+            $region_house = substr($region_house,0,strlen($region_house)-1);
+            $condition .= " AND h.region IN (".$region_house.")";
+
+        }
+
+        //  Метро +
+        if(isset(Yii::app()->session['undeground'])){
+
+            $metro_under = '';
+            foreach(Yii::app()->session['undeground'] as $val_under)
+            {
+                $metro_under .= trim($val_under.',');
+            }
+            $metro_under = substr($metro_under,0,strlen($metro_under)-1);
+            $condition .= " AND h.undeground IN (".$metro_under.")";
+
+        }
+
+        //  До метро +
+        if(isset(Yii::app()->session['metro_time'])){
+
+            $metro_to = intval(Yii::app()->session['metro_time']);
+            $condition .= " AND h.metro_time <=".$metro_to."";
+        }
+
+        // Пешком или транспортом до метро
+        if(isset(Yii::app()->session['metro_way'])) {
+
+            $metro_way = '';
+            foreach(Yii::app()->session['metro_way'] as $val_metro_way) {
+                $metro_way .= trim($val_metro_way.',');
+            }
+            $metro_way = substr($metro_way,0,strlen($metro_way)-1);
+            $condition .= " AND h.metro_way IN (".$metro_way.")";
+        }
+
+
+        // Класс дома
+        if(isset(Yii::app()->session['class_home'])) {
+
+            $class_home = '';
+            foreach(Yii::app()->session['class_home'] as $val_class_home) {
+                $class_home .= trim($val_class_home.',');
+            }
+            $class_home = substr($class_home,0,strlen($class_home)-1);
+            $condition .= " AND h.class_home IN (".$class_home.")";
+        }
+
+        // Тип желаемой учетной записи
+        if(isset(Yii::app()->session['type_account'])) {
+
+            $type_account = '';
+            foreach(Yii::app()->session['type_account'] as $val_type_account) {
+                $type_account .= trim($val_type_account.',');
+            }
+            $type_account = substr($type_account,0,strlen($type_account)-1);
+            $condition .= " AND u.type_account IN (".$type_account.")";
+        }
+
+
+
         #### -- Продать -> Строящаяся -> Квартира и Аппартаменты (Ф-1): 1 - 7 - 4(3)
         if((isset(Yii::app()->session['type_estate']) && (Yii::app()->session['type_estate'] == 4 || Yii::app()->session['type_estate'] == 3)) &&
            (isset(Yii::app()->session['operations'])  &&  Yii::app()->session['operations'] == 1) &&
@@ -289,15 +463,32 @@ class ResultController extends Controller {
                     r.parking      AS parking,
                     r.place_cars   AS placeCars,
                     r.covered_space AS coveredSpace,
+                    r.club_type    AS clubType,
+                    r.discount     AS discount,
+                    r.mortgage     AS mortgage,
+                    r.sanitare     AS sanitare,
+                    r.create_data  AS createData,
+                    r.deadline     AS deadline,
+                    r.developer    AS developer,
+                    r.fz_214       AS fz_214,
+                    r.finished     AS finished,
+                    r.photos       AS photos,
 
                     h.id           AS houseID,
                     h.house_number AS houseNumber,
                     h.floors       AS floors,
+                    h.district     AS district,
+                    h.region       AS region,
+                    h.undeground   AS undeground,
+                    h.metro_time   AS metroTime,
+                    h.metro_way    AS metroWay,
+                    h.class_home   AS classHome,
 
                     u.uid          AS userID,
                     u.sub_email    AS email,
                     u.last_name    AS lastName,
                     u.phone        AS phone,
+                    u.type_account AS typeAccount,
 
                     stage.name     AS stageName,
                     currency.name  AS currencyName,
@@ -318,6 +509,8 @@ class ResultController extends Controller {
               WHERE ".$condition."
               ORDER BY r.apart_id DESC";
 
+            $act = 1; // признак выбора виджета
+
         }
 
         //
@@ -331,6 +524,9 @@ class ResultController extends Controller {
            (isset(Yii::app()->session['market'])      &&  Yii::app()->session['market'] == 6))
         {
             // Новый SQL запрос
+            $sql = "SELECT * FROM real_estate";
+
+            $act = 2; // признак выбора виджета
         }
 
 
@@ -340,6 +536,9 @@ class ResultController extends Controller {
            (isset(Yii::app()->session['market'])      &&  Yii::app()->session['market'] == 6))
         {
             // Новый SQL запрос
+            $sql = "SELECT * FROM real_estate";
+
+            $act = 3; // признак выбора виджета
         }
 
 
@@ -350,6 +549,9 @@ class ResultController extends Controller {
            (isset(Yii::app()->session['market'])      &&  Yii::app()->session['market'] == 6))
         {
             // Новый SQL запрос
+            $sql = "SELECT * FROM real_estate";
+
+            $act = 4; // признак выбора виджета
         }
 
 
@@ -359,12 +561,18 @@ class ResultController extends Controller {
            (isset(Yii::app()->session['market'])      &&  Yii::app()->session['market'] == 6))
         {
             // Новый SQL запрос
+            $sql = "SELECT * FROM real_estate";
+
+            $act = 5; // признак выбора виджета
         }
 
 
+        $count=$connection->createCommand("SELECT COUNT(r.apart_id)
+                                           FROM real_estate AS r
+                                           INNER JOIN s_house AS h ON h.id = r.fk_house_id
+                                           INNER JOIN users AS u   ON r.fk_uid = u.uid
+                                           WHERE ".$condition."")->queryScalar();
 
-
-        $count=$connection->createCommand("SELECT COUNT(r.apart_id) FROM real_estate AS r WHERE ".$condition."")->queryScalar();
         $dataProvider = new CSqlDataProvider($sql, array(
             'keyField'=>'apartID',
             'totalItemCount'=>$count,
@@ -385,6 +593,8 @@ class ResultController extends Controller {
 
         $this->render('index',array(
             'dataProvider'=>!empty($dataProvider)?$dataProvider:null,
+            'act' => $act,
+
         ));
     }
 
--- protected/views/result/index.php
+++ protected/views/result/index.php
@@ -2,7 +2,7 @@
 
 
 
-if(isset($dataProvider)) {
+if(isset($dataProvider) && $act == 1) {
 
 
     $this->widget('zii.widgets.grid.CGridView', array(
@@ -22,11 +22,8 @@ if(isset($dataProvider)) {
                 'name'=>'houseID',
                 'header'=> 'Характеристика',
                 'value' => function($data,$row,$column){
-                           // $data - это объект модель для текущей стройки
-                           // $row - это порядковый номер строчки начиная с нуля
-                           // $column - объект колонки, объект класса http://www.yiiframework.com/doc/api/1.1/CDataColumn/
-                           // $this - объект колонки, объект класса http://www.yiiframework.com/doc/api/1.1/CDataColumn/
 
+                    $connection = Yii::app()->db;
 
                     switch($data["typeEstate"]) {
                          case 4 : $typeHouse = "квартира";     break;
@@ -47,10 +44,50 @@ if(isset($dataProvider)) {
                         case 3 : $window = "двор + улица"; break;
                         default: $window = "";             break;
                     }
-                    $balcony   = ($data["balcony"]!=0)?'есть':'нет';
-                    $parking   = ($data["parking"]!=0)?'есть':'нет';
-                    $placeCars = ($data["placeCars"]!=0)?'есть':'нет';
-                    $coveredSpace = ($data["coveredSpace"]!=0)?'есть':'нет';
+                    switch($data["sanitare"]) {
+                        case 1 : $sanitare = "раздельный";  break;
+                        case 2 : $sanitare = "совмещенный"; break;
+                        case 3 : $sanitare = " 2+ санузла"; break;
+                        default: $sanitare = "";            break;
+                    }
+                    switch($data["metroWay"]) {
+                        case 1 : $metroWay = "пешком";    break;
+                        case 2 : $metroWay = "на машине"; break;
+                    }
+                    switch($data["classHome"]) {
+                        case 1 : $classHome = "эконом";    break;
+                        case 2 : $classHome = "бизнес";    break;
+                        case 3 : $classHome = "элитный";   break;
+                    }
+                    switch($data["typeAccount"]) {
+                        case 1 : $typeAccount = "собственник";    break;
+                        case 2 : $typeAccount = "представитель собственника"; break;
+                        case 3 : $typeAccount = "риелтор";   break;
+                    }
+
+
+                    $district = $connection->createCommand("
+                        SELECT name FROM s_district WHERE fk_district = ".$data["district"]."")->queryRow();
+
+                    $region = $connection->createCommand("
+                        SELECT name FROM s_region WHERE id = ".$data["region"]."")->queryRow();
+
+                    $undeground = $connection->createCommand("
+                        SELECT name FROM s_undeground WHERE id = ".$data["undeground"]."")->queryRow();
+
+                    $balcony   = ($data["balcony"]!=0)?'есть':'нет'; //балкон
+                    $parking   = ($data["parking"]!=0)?'есть':'нет'; //паркинг
+                    $placeCars = ($data["placeCars"]!=0)?'есть':'нет'; //машиноместо
+                    $coveredSpace = ($data["coveredSpace"]!=0)?'есть':'нет'; //огороженая територия
+                    $clubType  = ($data["clubType"]!=0)?'есть':'нет'; //клубный тип
+                    $discount  = ($data["discount"]!=0)?'есть':'нет'; //акции и скидки
+                    $mortgage  = ($data["mortgage"]!=0)?'есть':'нет'; // ипотека
+                    $deadline  = ($data["deadline"]!=0)?$data["deadline"]:'нет'; // срок сдачи
+                    $developer = ($data["developer"]!='0')?$data["developer"]:'нет'; // застройщик
+                    $fz_214    = ($data["fz_214"]!=0)?'есть':'нет'; // регистрация нвостройки
+                    $finished  = ($data["finished"]!=0)?'есть':'нет'; // с отделкой
+                    $photos    = ($data["photos"]!=0)?'есть':'нет'; // с фото
+                    $metroTime = ($data["metroTime"]!=0)?$data["metroTime"]:'нет'; // до метро
 
 
                     return "<b>Тип недвижимости:</b> ".$typeHouse."<br>".
@@ -63,17 +100,34 @@ if(isset($dataProvider)) {
                             "<b>Цена объекта:</b> ".$data["price"]."<br>".
                             "<b>Тип стен:</b> ".$data["typeHouse"]."<br>".
                             "<b>Стадия строительства:</b> ".$data["stageName"]."<br>".
-                            "<b>Срок сдачи:</b> ".$data["deadline"]."<br>".
                             "<b>Email:</b> ".$data["email"]."<br>".
-                            "<b>Email:</b> ".$data["lastName"]."<br>".
+                            "<b>Имя:</b> ".$data["lastName"]."<br>".
                             "<b>Phone:</b> ".$data["phone"]."<br>".
                             "<b>Window:</b> ".$window."<br>".
                             "<b>Balcony:</b> ".$balcony."<br>".
                             "<b>Parking:</b> ".$parking."<br>".
                             "<b>Машиноместо:</b> ".$placeCars."<br>".
                             "<b>Закрытая територия:</b> ".$coveredSpace."<br>".
-                            "<b>Photos:</b> ".$data["photos"]."<br>".
-                            "<b>City:</b> ".$data["cityName"]."<br>".
+                            "<b>Клубный тип:</b> ".$clubType."<br>".
+                            "<b>Акции и скидки:</b> ".$discount."<br>".
+                            "<b>Ипотека:</b> ".$mortgage."<br>".
+                            "<b>Санузел:</b> ".$sanitare."<br>".
+                            "<b>Дата создания:</b> ".$data["createData"]."<br>".
+                            "<b>Срок сдачи:</b> ".$deadline."<br>".
+                            "<b>Застройщик:</b> ".$developer."<br>".
+                            "<b>Fz_214:</b> ".$fz_214."<br>".
+                            "<b>С отделкой:</b> ".$finished."<br>".
+                            "<b>Photos:</b> ".$photos."<br>".
+
+                            "<b>Этажность дома:</b> ".$data["floors"]."<br>".
+                            "<b>Город:</b> ".$data["cityName"]."<br>".
+                            "<b>Округ:</b> ".$district['name']."<br>".
+                            "<b>Регион:</b> ".$region['name']."<br>".
+                            "<b>Метро:</b> ".$undeground['name']."<br>".
+                            "<b>До метро:</b> ".$metroTime." мин.<br>".
+                            "<b>Как добраться до метро:</b> ".$metroWay."<br>".
+                            "<b>Класс дома:</b> ".$classHome."<br>".
+                            "<b>Тип аккаунта:</b> ".$typeAccount."<br>".
                             "<b>Street:</b> ".$data["streetName"]."<br>";
 
                     },
@@ -99,13 +153,28 @@ if(isset($dataProvider)) {
         ),
         'pagerCssClass'=>'pagination',
     ));
-} else {
-
-    echo 'Error';
 }
+else if(isset($dataProvider) && $act == 2) {
 
+    // Новый виджет для формы 2
+    print 2;
+}
+else if(isset($dataProvider) && $act == 3) {
 
+    // Новый виджет для формы 3
+    print 3;
+}
+else if(isset($dataProvider) && $act == 4) {
 
+    // Новый виджет для формы 4
+    print 4;
+
+}
+else  {
+
+    // Новый виджет для формы 5
+    print 5;
+}
 
 
 
